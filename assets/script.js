bfa591482435d6755064f5bc125b121bef9695e9
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-24 18:52:30 -0800

    working on psudocode

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// search bar for location 
+// Weather API 
+// add local storage to save previous search 
+// Weather need to include full UV index 
+// depending on the weather UV index color will need to change > use css, three different colors
+// Weather must include 5 day forcast 
