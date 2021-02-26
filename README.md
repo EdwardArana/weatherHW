4eeba266c677eedcfea4e623171bd0921ba333c3
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-25 17:35:59 -0800

    working on js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weatherHW
+
+This website was designed to get real time weather updates. This website has a search bar feature that will save previous logged information (Example "Texas", "Utah" etc.) and give you real time weather updates on the location typed in. I am using "Open Weather API" for this webiste in order to get the weather, you can use the City name and State Codes for your search. 
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,47 @@
-// search bar for location 
-// Weather API 
-// add local storage to save previous search 
-// Weather need to include full UV index 
-// depending on the weather UV index color will need to change > use css, three different colors
-// Weather must include 5 day forcast 
+var searchFormEl = $("#search-input");
+var button = $("search-button");
+var current = $("#today");
+var temp = $("weather");
+
+function handleSearchFormSubmit() {
+    var city = searchFormEl.val()
+
+    var urlRequest = "https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid=40f3a66d546be5c27e5a38084e2d3425"
+
+    $.ajax({
+        url:urlRequest,
+        method: "GET",
+    }).then(function(response) {
+        console.log("Ajax Reponse \n----------");
+        console.log(response);
+
+    });
+}
+
+button.on("click", () => {
+    handleSearchFormSubmit()
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 40f3a66d546be5c27e5a38084e2d3425
