8560f424f8c2383131f77e85eb237a92868c42fc
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-25 18:06:05 -0800

    fixing the js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
+// Var section
+
 var searchFormEl = $("#search-input");
-var button = $("search-button");
+var button = $("#search-button");
 var current = $("#today");
-var temp = $("weather");
+var temprature = $("#weather");
+
 
 function handleSearchFormSubmit() {
+    
+    // API for weather
+    
     var city = searchFormEl.val()
 
-    var urlRequest = "https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid=40f3a66d546be5c27e5a38084e2d3425"
+    var urlRequest = `https://api.openweathermap.org/data/2.5/forecast?q=${city},{state code}&units=imperial&appid=40f3a66d546be5c27e5a38084e2d3425`
 
     $.ajax({
         url:urlRequest,
         method: "GET",
-    }).then(function(response) {
+    }).then(function (response) {
         console.log("Ajax Reponse \n----------");
         console.log(response);
+        current.text(new Date().getDate())
+        temprature.text(response.list[0].main.temp + " F")
 
     });
 }
@@ -28,7 +36,10 @@ button.on("click", () => {
 
 
 
-
+//add local storage to save previous search
+//weather need to include full UV index
+// depending on the weather UV index color will need to change color based on weather condition
+//weather must include 5 day forecast
 
 
 
