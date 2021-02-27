f02d942feacbcf79505f3c32869977ee1c1baeca
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-26 18:23:45 -0800

    js work

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ function handleSearchFormSubmit() {
     
     var city = searchFormEl.val()
     console.log(city);
-    var urlRequest = `https://api.openweathermap.org/data/2.5/forecast?q=${city},{state code}&appid=40f3a66d546be5c27e5a38084e2d3425`
+    var urlRequest = `https://api.openweathermap.org/data/2.5/forecast/?q=${city},{state code}&units=imperial&appid=40f3a66d546be5c27e5a38084e2d3425`
 
    
     $.ajax({
@@ -26,7 +26,8 @@ function handleSearchFormSubmit() {
 
         for(const data of response.list) {
             console.log(data);
-            if(data.dt_txt=== "2021-02-26 12:00:00") {
+            if(data.dt_txt.indexOf("12:00:00")!== -1) {
+                console.log(data.dt_txt);
                  current.text(new Date().getDate())
                  temprature.text(data.main.temp + " F")
             }
@@ -46,10 +47,13 @@ button.on('click', () => {
 //weather need to include full UV index
 // depending on the weather UV index color will need to change color based on weather condition
 //weather must include 5 day forecast
-// why isnt api working 
-// monday-friday, "hard code"
 // set 3 if statmets to loop 
 // second apiu request for other dash req
 
 
+
 // 40f3a66d546be5c27e5a38084e2d3425
+
+
+
+// inside IF create DIV, then create section for temp,hum,uv, and icon "card". then i need to append all of these into the div i created, then append the div with everything in it on page. 
