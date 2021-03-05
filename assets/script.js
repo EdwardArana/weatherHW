b8821fca29b510fcede550397e8aa388e0c504a2
Edward Arana <Edwardaranadlt@gmail.com>
2021-03-05 10:03:21 -0800

    working on the js

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ function handleSearchFormSubmit() {
     
     var city = searchFormEl.val()
     console.log(city);
-    var urlRequest = `https://api.openweathermap.org/data/2.5/forecast/?q=${city},{state code}&units=imperial&appid=40f3a66d546be5c27e5a38084e2d3425`
+    var urlRequest = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=40f3a66d546be5c27e5a38084e2d3425`
 
     
    
@@ -22,40 +22,56 @@ function handleSearchFormSubmit() {
     }).then(function (response) {
         console.log('Ajax Reponse \n----------');
         console.log(response);
-        current.text(new Date().getDate())
-        temprature.text(response.list[0].main.temp + " F")
-
-        for(const data of response.list) {
-            console.log(data.dt_txt);
-            console.log(data.dt_txt.includes("12:00:00"));
-            const date = new Date();
+        $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=40f3a66d546be5c27e5a38084e2d3425`,
+            method: 'GET'
+        }).then(function(response2) {
+            console.log(response2)
+            const card = $('<div>').addClass("card");
+            // const img = $('<img>').addClass("card-img-top");
+            const cardBody = $('<div>').addClass("card-body");
+            const cardTitle =$('<h5>').addClass("card-title").text(response.name);
+            const cardUlEl = $("<ul>").addClass("list-group list-group-flush");
+            const cardListItem1 = $("<li>").addClass("list-group-item").text(response.humidity);
+            const cardListItem2 = $("<li>").addClass("list-group-item").text(response.main.temp);
+            const cardListItem3 = $("<li>").addClass("list-group-item").text(response2.current.uvi);
+            cardUlEl.append(cardListItem1, cardListItem2, cardListItem3);
+            cardBody.append(cardTitle)
+            cardBody.append(cardUlEl)
+            card.append(cardBody)
+            temprature.append(card)
             
-            if() {
-                console.log(data.dt_txt);
-                //  current.text(new Date().getDate())
-                //  temprature.text(data.main.temp + " F")
-                // for( let i = 0; i < 5; i++) {
-                    const card = $('<div>').addClass("card");
-                    const img = $('<img>').addClass("card-img-top");
-                    const cardBody = $('<div>').addClass("card-body");
-                    const cardTitle =$('<h5>').addClass("card-title").text(data.dt_txt);
-                    const cardText = $('<p>').addClass("card-text").text("Hello");
+        })
+                    // const cardText = $('<p>').addClass("card-text").text("Hello");
+        // current.text(new Date().getDate())
+        // temprature.text(response.list[0].main.temp + " F")
+
+        // for(const data of response.list) {
+        //     console.log(data.dt_txt);
+        //     console.log(data.dt_txt.includes("12:00:00"));
+        //     const date = new Date();
+            
+        //     if() {
+        //         console.log(data.dt_txt);
+        //         //  current.text(new Date().getDate())
+        //         //  temprature.text(data.main.temp + " F")
+        //         // for( let i = 0; i < 5; i++) {
                     
                     
-                    // img.append(cardBody)
-                    card.append(img)
-                    cardBody.append(cardTitle, cardText)
-                    card.append(cardBody)
-                    $(".card-group").append(card);
-                // }
-            }
+        //             // img.append(cardBody)
+        //             card.append(img)
+        //             cardBody.append(cardTitle, cardText)
+        //             card.append(cardBody)
+        //             $(".card-group").append(card);
+        //         // }
+        //     }
             
            
     
     
             
 
-        }
+        // }
     });
 
 }
